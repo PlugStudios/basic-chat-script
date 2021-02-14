873dc14bad92d863c5c3ecb9964bc58321e55567
RoyalKlutch <76459861+RoyalKlutch@users.noreply.github.com>
2021-02-14 15:56:57 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Resource Metadata
+fx_version '44febabe-d386-4d18-afbe-5e627f4af937'
+games { 'gta5' }
+
+author 'RoyalKlutch'
+description 'Chat Script'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'client.lua'
+}

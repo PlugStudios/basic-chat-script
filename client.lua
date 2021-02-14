8d00a2b3339ac5e37592c03fe4c10faecacaa566
RoyalKlutch <76459861+RoyalKlutch@users.noreply.github.com>
2021-02-14 15:56:32 -0500

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,13 @@
+-- Video Tutorial https://www.youtube.com/watch?v=3po2_YnGk1U
+-- DO NOT EDIT
+RegisterCommand('discord', function()
+    TriggerEvent('chat:addMessage',{
+     color: {255,255,0}
+     multiline true,
+     args = {'SERVER NAME', 'SYSTEM MSG'}
+    })
+end) -- /discord
+
+-- COLORS -- 
+-- Red = 255,0,0
+-- Yellow = 255,255,0
